6103937e5203ca8289c60e5e3d404a6aa87d1776
L16H7 <lightcode2018@gmail.com>
2025-05-16 18:09:16 +0000

    prompt construction

--- GRPO-hack/inference.py
+++ GRPO-hack/inference.py
@@ -1,10 +1,15 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-# BASE_MODEL_NAME = "Qwen/Qwen3-0.6B-Base"
-BASE_MODEL_NAME = "/root/arc/ARC-AGI/checkpoint-2800"
+BASE_MODEL_NAME = "Qwen/Qwen3-0.6B"
+# BASE_MODEL_NAME = "/root/arc/ARC-AGI/checkpoint-2800"
 
 # load the tokenizer and the model
-base_model_tokenizer = AutoTokenizer.from_pretrained(BASE_MODEL_NAME)
+
+# load the tokenizer and the model
+base_model_tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen3-0.6B")
+base_model_tokenizer.pad_token = '<|endoftext|>'
+base_model_tokenizer.eos_token_id = 151643
+
 base_model = AutoModelForCausalLM.from_pretrained(
     BASE_MODEL_NAME,
     torch_dtype="auto",
@@ -12,22 +17,23 @@ base_model = AutoModelForCausalLM.from_pretrained(
 )
 
 def generate_text_base(messages, max_new_tokens=500):
-    text = base_model_tokenizer.apply_chat_template(
-        messages,
-        tokenize=False,
-        add_generation_prompt=True,
-    )
+    # text = base_model_tokenizer.apply_chat_template(
+    #     messages,
+    #     tokenize=False,
+    #     add_generation_prompt=True,
+    # )
+    text = messages
 
     model_inputs = base_model_tokenizer([text], return_tensors="pt").to(base_model.device)
 
     # conduct text completion
     generated_ids = base_model.generate(
         **model_inputs,
-        max_new_tokens=max_new_tokens
+        max_new_tokens=max_new_tokens,
     )
     output_ids = generated_ids[0].tolist()
 
-    content = base_model_tokenizer.decode(output_ids[len(model_inputs[0]) - 20:], skip_special_tokens=False).strip("\n")
+    content = base_model_tokenizer.decode(output_ids, skip_special_tokens=False).strip("\n")
 
     return {
         "output_ids": output_ids,
@@ -669,6 +675,148 @@ challenge =  {
     ]
 }
 
+challenge2 = {
+        "train": [
+            {
+                "input": [
+                    [
+                        7,
+                        9
+                    ],
+                    [
+                        4,
+                        3
+                    ]
+                ],
+                "output": [
+                    [
+                        7,
+                        9,
+                        7,
+                        9,
+                        7,
+                        9
+                    ],
+                    [
+                        4,
+                        3,
+                        4,
+                        3,
+                        4,
+                        3
+                    ],
+                    [
+                        9,
+                        7,
+                        9,
+                        7,
+                        9,
+                        7
+                    ],
+                    [
+                        3,
+                        4,
+                        3,
+                        4,
+                        3,
+                        4
+                    ],
+                    [
+                        7,
+                        9,
+                        7,
+                        9,
+                        7,
+                        9
+                    ],
+                    [
+                        4,
+                        3,
+                        4,
+                        3,
+                        4,
+                        3
+                    ]
+                ]
+            },
+            {
+                "input": [
+                    [
+                        8,
+                        6
+                    ],
+                    [
+                        6,
+                        4
+                    ]
+                ],
+                "output": [
+                    [
+                        8,
+                        6,
+                        8,
+                        6,
+                        8,
+                        6
+                    ],
+                    [
+                        6,
+                        4,
+                        6,
+                        4,
+                        6,
+                        4
+                    ],
+                    [
+                        6,
+                        8,
+                        6,
+                        8,
+                        6,
+                        8
+                    ],
+                    [
+                        4,
+                        6,
+                        4,
+                        6,
+                        4,
+                        6
+                    ],
+                    [
+                        8,
+                        6,
+                        8,
+                        6,
+                        8,
+                        6
+                    ],
+                    [
+                        6,
+                        4,
+                        6,
+                        4,
+                        6,
+                        4
+                    ]
+                ]
+            }
+        ],
+        "test": [
+            {
+                "input": [
+                    [
+                        3,
+                        2
+                    ],
+                    [
+                        7,
+                        8
+                    ]
+                ]
+            }
+        ]
+    }
 def grid_to_string(grid):
     # Convert each row to a string representation with brackets
     rows_str = [str(row) for row in grid]
@@ -676,7 +824,7 @@ def grid_to_string(grid):
     grid_str = "\n".join(rows_str)
     return grid_str
 
-prompt = "You are participating in a puzzle solving competition. You are an expert at solving puzzles.\n\nBelow is a list of input and output pairs with a pattern. Your goal is to identify the pattern or transformation in the training examples that maps the input to the output, then apply that pattern to the test input to give a final output.\n\nRespond in the format of the training output examples\n\n--Training Examples--"
+prompt = "<|im_start|>user\nYou are participating in a puzzle solving competition. You are an expert at solving puzzles.\n\nBelow is a list of input and output pairs with a pattern. Your goal is to identify the pattern or transformation in the training examples that maps the input to the output, then apply that pattern to the test input to give a final output.\n\nRespond in the format of the training output examples\n\n--Training Examples--"
 
 for i, input_example in enumerate(challenge["train"]):
     prompt += f"\n--Example {i}-- \n\n INPUT: \n\n"
@@ -687,8 +835,11 @@ for i, input_example in enumerate(challenge["train"]):
 
 prompt += "\n\n--End of Training Examples--\n\n--Test Input--\n"
 prompt += grid_to_string(challenge["test"][0]['input'])
-prompt += "\n\n--End of Test Input--\n\nYour response:"
+prompt += "\n\n--End of Test Input--\n\nProvide answer in <answer> tag precisely so that I can parse and validate.<|im_end|>\n<|im_start|>assistant"
 
-response = generate_text_base(messages=[{ "content": prompt, "role": "user" }], max_new_tokens=4000)
+# response = generate_text_base(messages=[{ "content": prompt, "role": "user" }], max_new_tokens=4000)
+response = generate_text_base(messages=prompt, max_new_tokens=4000)
 print(response['content'])
 # print(prompt)
+import pdb
+pdb.set_trace()
