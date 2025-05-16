f0ce025dac295e7d897ee4786479f4d9a91f010e
L16H7 <lightcode2018@gmail.com>
2025-05-15 22:09:35 -0700

    config

--- GRPO-hack/sft_arc.py
+++ GRPO-hack/sft_arc.py
@@ -1,8 +1,12 @@
 import torch
+import wandb
+
 from datasets import load_dataset
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from trl import SFTTrainer, SFTConfig
 
+
+wandb.init(project="SFT_ARC")
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -20,10 +24,11 @@ tokenizer.pad_token = '<|endoftext|>'
 tokenizer.eos_token_id = 151643
 
 dataset = load_dataset('tttx/r1-arcagi-successful-trajectories')['train']
+eval_dataset = load_dataset('tttx/r1-arcagi-successful-trajectories')['test']
 
 training_args = SFTConfig(
     output_dir="./",
-    run_name="arc-sft/test-sft",
+    run_name="test-sft",
     learning_rate=5e-6,
     adam_beta1=0.9,
     adam_beta2=0.99,
@@ -39,13 +44,16 @@ training_args = SFTConfig(
     save_steps=100,
     max_grad_norm=0.2,
     report_to="wandb",
+    eval_steps=20,
+    eos_token='<|endoftext|>',
 )
 
 trainer = SFTTrainer(
     model=model,
     processing_class=tokenizer,
     train_dataset=dataset,
-    args=training_args
+    args=training_args,
+    eval_dataset=eval_dataset,
 )
 
 trainer.train()
